d1ff8bd35b0624ed6c5785adbf7ca4a7f0847cdc
Igor Ribeiro Lobo <igorribeirolobo@gmail.com>
2017-09-05 22:13:10 -0300

    try catch
    codigo try catch

--- src/aula/Main.java
+++ src/aula/Main.java
@@ -6,14 +6,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		funcoes funcao = new funcoes();
-		 int a, b;
+		int a = 0, b = 0;
+		boolean executa = true;
+		while(executa)
+		{
+		   try {
 		    a = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor de A: "));
-		    
+		    executa = false;
+		   }
+		   catch(NumberFormatException e)
+		   {
+			   JOptionPane.showMessageDialog(null, "informe um numero válido, erro: "+ e.toString());
+           }
+		}
+		executa = true;
+		while(executa)
+		   try
+		   {
 		    b = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor de B: "));
-		    
-		    funcao.RecebeNumero(a, b);
-		    JOptionPane.showMessageDialog(null, "A soma retronou um resultado que é: "+ funcao.Somar());
-		  
+		    executa = false;
+
+		   }
+		   catch(NumberFormatException e)
+		   {
+			   JOptionPane.showMessageDialog(null,"informe um numero válido, erro: "+ e.toString());
+		   }
+	    funcao.RecebeNumero(a, b);
+	    JOptionPane.showMessageDialog(null, "A soma retronou um resultado que é: "+ funcao.Somar());
 	}
 
 }
